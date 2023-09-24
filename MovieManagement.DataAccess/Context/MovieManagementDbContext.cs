f02723a58ee93388e278ff0f78e059b81e15d3bc
Nahid-Hassan-Sourav <nahidhasansourav05@gmail.com>
2023-09-24 17:52:39 +0600

    Migration created

---
+++ MovieManagement.DataAccess/Context/MovieManagementDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.DataAccess.Context
+{
+    public class MovieManagementDbContext : DbContext
+    {
+        public MovieManagementDbContext(DbContextOptions<MovieManagementDbContext> options) : base(options) { }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Biography> Biographies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor>().HasData(
+                new Actor { Id = 1, FirstName = "Raisa", LastName = "Islam" },
+                new Actor { Id = 2, FirstName = "Afrida", LastName = "Hasnat" },
+                new Actor { Id = 3, FirstName = "Rahat", LastName = "Islam" }
+                );
+
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie { Id = 1, Name = "Jawan", Description = "In SKS Tower", ActorId = 1 },
+                new Movie { Id = 2, Name = "MI7", Description = "In Bashundhara City", ActorId = 3 },
+                new Movie { Id = 3, Name = "Surongo", Description = "In Sony Sqaure", ActorId = 2 }
+                );
+        }
+    }
+}
